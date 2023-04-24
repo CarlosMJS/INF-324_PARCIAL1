dc6a0602841f8609d04a705e0810f41d12310e69
CarlosMJS <94412169+CarlosMJS@users.noreply.github.com>
2023-04-24 12:35:31 -0400

    Delete ej9 directory

--- ej9/ejercicio9-INF324/eliminar.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "conexion.inc.php";
-$CI=$_GET["CI"];
-$sql="delete from persona where CI=$CI";
-mysqli_query($con, $sql);
-?>
-<img  width="300px" src="./img/eliminar.jpg"/>
-<br/>
-<a href="inicial.php">Volver</a>
