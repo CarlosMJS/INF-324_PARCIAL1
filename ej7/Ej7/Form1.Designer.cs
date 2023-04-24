3a1cb74d0cd256f64a73a42b3649f6f142077ea8
CarlosMJS <94412169+CarlosMJS@users.noreply.github.com>
2023-04-24 12:50:01 -0400

    Add files via upload

---
+++ ej7/Ej7/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Ej7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CI = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Nombre_completo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fechaNaci = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.telefono = new System.Windows.Forms.TextBox();
+            this.departamento = new System.Windows.Forms.TextBox();
+            this.insert_data_Click1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(830, 291);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 347);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 50);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "INSERTAR PERSONA\r\n\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // CI
+            // 
+            this.CI.Location = new System.Drawing.Point(336, 393);
+            this.CI.Name = "CI";
+            this.CI.Size = new System.Drawing.Size(100, 20);
+            this.CI.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 388);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 25);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "CI:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Nombre_completo
+            // 
+            this.Nombre_completo.Location = new System.Drawing.Point(336, 438);
+            this.Nombre_completo.Name = "Nombre_completo";
+            this.Nombre_completo.Size = new System.Drawing.Size(100, 20);
+            this.Nombre_completo.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 433);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 25);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "NOMBRE:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 478);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(280, 25);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "FECHA DE NACIMIENTO:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // fechaNaci
+            // 
+            this.fechaNaci.Location = new System.Drawing.Point(336, 483);
+            this.fechaNaci.Name = "fechaNaci";
+            this.fechaNaci.Size = new System.Drawing.Size(100, 20);
+            this.fechaNaci.TabIndex = 26;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(477, 387);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 25);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "TELEFONO:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(477, 433);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(206, 25);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "DEPARTAMENTO:";
+            // 
+            // telefono
+            // 
+            this.telefono.Location = new System.Drawing.Point(710, 392);
+            this.telefono.Name = "telefono";
+            this.telefono.Size = new System.Drawing.Size(100, 20);
+            this.telefono.TabIndex = 29;
+            // 
+            // departamento
+            // 
+            this.departamento.Location = new System.Drawing.Point(710, 438);
+            this.departamento.Name = "departamento";
+            this.departamento.Size = new System.Drawing.Size(100, 20);
+            this.departamento.TabIndex = 30;
+            // 
+            // insert_data_Click1
+            // 
+            this.insert_data_Click1.Location = new System.Drawing.Point(482, 483);
+            this.insert_data_Click1.Name = "insert_data_Click1";
+            this.insert_data_Click1.Size = new System.Drawing.Size(159, 36);
+            this.insert_data_Click1.TabIndex = 31;
+            this.insert_data_Click1.Text = "INSERTAR";
+            this.insert_data_Click1.UseVisualStyleBackColor = true;
+            this.insert_data_Click1.UseWaitCursor = true;
+            this.insert_data_Click1.Click += new System.EventHandler(this.insert_data_Click1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(668, 483);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 34);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "actualizar tabla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 583);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.insert_data_Click1);
+            this.Controls.Add(this.departamento);
+            this.Controls.Add(this.telefono);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.fechaNaci);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Nombre_completo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CI);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox CI;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Nombre_completo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox fechaNaci;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox telefono;
+        private System.Windows.Forms.TextBox departamento;
+        private System.Windows.Forms.Button insert_data_Click1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ej7/Ej7/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ej7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ServiceReference1.WebServiceSoapClient ws = new ServiceReference1.WebServiceSoapClient();
+            DataSet ds = new DataSet();
+            ds = ws.personaid();
+            dataGridView1.DataSource = ds.Tables[0];
+
+            DataGridViewButtonColumn btn_eliminar = new DataGridViewButtonColumn();
+            dataGridView1.Columns.Add(btn_eliminar);
+            btn_eliminar.HeaderText = "Acciones";
+            btn_eliminar.Text = "Eliminar";
+            btn_eliminar.Name = "btn_eliminar";
+            btn_eliminar.UseColumnTextForButtonValue = true;
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            var senderGrid = (DataGridView)sender;
+            if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn && e.RowIndex >= 0)
+            {
+                ServiceReference1.WebServiceSoapClient ws = new ServiceReference1.WebServiceSoapClient();
+                int id = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[1].Value);
+                ws.baja(id);
+                dataGridView1.Rows.RemoveAt(e.RowIndex);
+            }
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insert_data_Click1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.WebServiceSoapClient ws = new ServiceReference1.WebServiceSoapClient();
+            ws.alta(Convert.ToInt32(CI.Text),Nombre_completo.Text,fechaNaci.Text,telefono.Text,departamento.Text);
+            DataTable dato = new DataTable();
+            dato = dataGridView1.DataSource as DataTable;
+            DataRow datarow;
+            datarow = dato.NewRow();
+            datarow["CI"] = CI.Text;
+            datarow["Nombre_completo"] = Nombre_completo.Text;
+            datarow["fechaNaci"] = fechaNaci.Text;
+            datarow["telefono"] = telefono.Text;
+            datarow["departamento"] = departamento.Text;
+            dato.Rows.Add(datarow);  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.WebServiceSoapClient ws = new ServiceReference1.WebServiceSoapClient();
+            DataSet ds = ws.personaid();
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+    }
+}
